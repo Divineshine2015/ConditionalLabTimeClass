cfee46ff8d3837e9a2734b8e1af440e4ed020b9b
Jon Reed <mr.is@outlook.com>
2015-03-25 14:49:38 -0400

    3_25_15 Conditional Lab TimeClass
    3_25_15 Conditional Lab TimeClass completed

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.DateTime;
+
+namespace ConditionalLabTimeClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime currentTime =  DateTime.UtcNow;
+            int seconds =Convert.ToInt32( currentTime.ToString("ss"));
+            Console.WriteLine(seconds);
+            //Console.WriteLine(currentTime.ToString(":ss"));
+
+            switch (seconds)
+            {
+                case 0:
+                    Console.WriteLine("The New Minute is just Beginning");
+                    break;
+                case 15:
+                    Console.WriteLine("We're one quarter done");
+                    break;
+                case 30:
+                    Console.WriteLine("half way there");
+                    break;
+                case 45:
+                    Console.WriteLine("Getting close to done");
+                    break;
+
+                default:
+                    Console.WriteLine("Working on it");
+                    break;
+            }
+        }
+    }
+}
